7b68292ca2f8ee882c7c1f65ab49c3501fd99e1c
Piairecarre <cymdit@orange.fr>
2018-04-10 01:29:13 +0200

    Semaine 2 - Learn_ruby-master -02_ calculator ajouté

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,18 @@
 #write your code here
+def add(a, b)
+	a+b
+end
+
+def subtract(a, b)
+	a-b
+end
+
+def sum(arr)
+	s = 0
+	if arr.empty?
+		return 0
+	else		
+		arr.each {|val| s += val}
+	end
+	return s
+end
